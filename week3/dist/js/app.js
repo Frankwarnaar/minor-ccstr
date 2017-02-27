e6be3b9f7ef355e886a112a31defbb2d59544052
Frank Warnaar <f.warnaar@viriciti.com>
2017-02-27 19:08:55 +0100

    new nav without classes

---
+++ week3/dist/js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*jshint esversion: 6 */
+(function () {
+    // 'use strict';
+    // window.addEventListener('hashchange', () => {
+    //     const body = document.querySelector('body');
+    //     body.classList.remove('hashChanged');
+    //     setTimeout(() => {
+    //         body.classList.add('hashChanged');
+    //     }, 1);
+    // });
+})();
